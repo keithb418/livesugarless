58f1b5a811b0f4b4a9beee54c1195482747b03c5
Keith Brown <keithb418@gmail.com>
2016-03-12 10:01:30 -0500

    updating the home page image to resize with the window

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -12,5 +12,42 @@ define((require) => {
         }])
         .controller('homeCtrl', [() => {
             
-        }]);
+        }])
+        .directive('resizeImg', ($window) => {
+            return ($scope, $el, attr) => {
+                let w = angular.element($window);
+                let $header = angular.element('header');
+                let $footer = angular.element('footer');
+                let headerHeight = $header.outerHeight();
+                let footerHeight = $footer.outerHeight();
+                let height = w.outerHeight() - (headerHeight + footerHeight);
+                
+                $scope.$watch(() => {
+                    return {
+                        'height': w.outerHeight(),
+                        'width': w.outerWidth()
+                    };
+                }, (newValue, oldValue) => {
+                    $scope.updateImg = () => {
+                        var style = {};
+                        
+                        if (newValue.width >= newValue.height) {
+                            return {
+                                height: 'auto',
+                                width: '100%'
+                            }
+                        }
+                        
+                        return {
+                            height: '100%',
+                            width: 'auto'
+                        };
+                    }
+                }, true);
+                
+                w.bind('resize', () => {
+                    $scope.$apply();
+                });
+            };    
+        });
 });
